b77b17517e02cc179a3835123850ed7e7badb6ba
lyt <taoyxzh@163.com>
2026-09-04 11:11:48 +0800

    release: v0.5.2

--- backend/server.py
+++ backend/server.py
@@ -414,7 +414,7 @@ class Server(ThreadingHTTPServer):
 
 
 class Handler(BaseHTTPRequestHandler):
-    server_version = "nginx-manager/0.5.1"
+    server_version = "nginx-manager/0.5.2"
     settings: SettingsStore = None  # type: ignore
     data_dirs: dict = {}
     controller: NginxController = None  # type: ignore
